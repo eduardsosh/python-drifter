6db5eaa1e21ed46001503c0730e71fa993dac842
kristiansrasins <rasinskristians@gmail.com>
2023-12-10 18:57:43 +0200

    track movement

---
+++ client/main.py
@@ -0,0 +1,39 @@
+import pygame
+import sys
+def background(color, image, position):
+    canvas.fill(color)
+    canvas.blit(image, dest = position)
+def move(image, position, x ,y):
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_s]:
+        y -= 5
+    if keys[pygame.K_w]:
+        y += 5
+    if keys[pygame.K_a]:
+        x += 5
+    if keys[pygame.K_d]:
+        x -= 5
+    return x, y
+
+
+pygame.init()
+color = (255, 255, 255)
+x = 0
+y = 0
+position = (x, y)
+canvas = pygame.display.set_mode((500, 500))
+
+pygame.display.set_caption("My Board") 
+image = pygame.image.load("bilde.png")
+
+exit = False
+  
+while not exit:
+    background(color, image, position)
+    x, y = move(image, position, x, y)
+    for event in pygame.event.get(): 
+        if event.type == pygame.QUIT: 
+            exit = True
+    position = (x, y)
+    pygame.display.update()
+    pygame.time.Clock().tick(60)
